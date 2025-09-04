d0619e25cf3f7ff5f48117774ef306feff1ec8e9
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-09-04 17:54:46 -0600

    refactor: add new component index files for avatars, cards, dialog, inputs, and navigation

---
+++ src/components/core/inputs/index.tsx
@@ -0,0 +1,7 @@
+// src/components/core/inputs/index.ts
+// src/components/core/inputs/index.ts
+export { useInputField, usePasswordInput, validators } from './hooks';
+export { InputField } from './InputField';
+export { InputFieldDemo } from './InputFieldDemo';
+export type { InputFieldProps, InputState } from './types';
+
