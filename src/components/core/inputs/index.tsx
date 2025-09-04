530793e0fce1dc2a592b64360781736a2ff54b53
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-09-04 17:56:10 -0600

    refactor: remove unused InputFieldDemo export from inputs index

--- src/components/core/inputs/index.tsx
+++ src/components/core/inputs/index.tsx
@@ -2,6 +2,5 @@
 // src/components/core/inputs/index.ts
 export { useInputField, usePasswordInput, validators } from './hooks';
 export { InputField } from './InputField';
-export { InputFieldDemo } from './InputFieldDemo';
 export type { InputFieldProps, InputState } from './types';
 
