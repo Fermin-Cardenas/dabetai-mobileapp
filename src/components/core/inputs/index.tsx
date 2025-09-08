2ff14cd491e078fedf25899097ee4b2d8fb06816
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-09-07 18:32:48 -0600

    feat: add PickerField component for enhanced input handling
    - Add new PickerField component for better picker interactions
    - Update input component exports to include PickerField
    - Improve input component architecture

--- src/components/core/inputs/index.tsx
+++ src/components/core/inputs/index.tsx
@@ -1,6 +1,6 @@
 // src/components/core/inputs/index.ts
-// src/components/core/inputs/index.ts
 export { useInputField, usePasswordInput, validators } from './hooks';
 export { InputField } from './InputField';
+export { PickerField } from './PickerField';
 export type { InputFieldProps, InputState } from './types';
 
