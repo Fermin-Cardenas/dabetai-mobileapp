792e92d8fa0040fead52e25aa40c7752c1774055
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-09-11 01:32:35 -0600

    refactor: update navigation exports

--- src/components/core/navigation/index.tsx
+++ src/components/core/navigation/index.tsx
@@ -2,3 +2,8 @@
 export { Header } from './Header';
 export { NavigationBar } from './NavigationBar';
 export { NavigationButton } from './NavigationButton';
+export { Pills } from './Pills';
+export { SegmentedControl } from './SegmentedControl';
+export { Tabs } from './Tabs';
+export { TabsControl } from './TabsControl';
+
