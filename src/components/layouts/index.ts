c2d8651a7f26684ff8b40777aeda2cdc8072ae23
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-09-11 01:35:37 -0600

    refactor: update layout exports

--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,5 +1,4 @@
 // src/components/layouts/index.ts
 export { default as AppLayout } from './AppLayout';
-export { default as ChatLayout } from './ChatLayout';
 export { default as OnboardingLayout } from './OnboardingLayout';
 
